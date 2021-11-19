4e73df81bca85c671041c421f7a981a2ca1c8120
Luís Ventuzelos <luisventuzelos@gmail.com>
2021-11-19 20:45:33 +0000

    changed data mock

--- src/app.js
+++ src/app.js
@@ -20,36 +20,27 @@ app.get("/welcome", auth, (req, res) => {
 
 app.post("/register", async (req, res) => {
 
-  // Our register logic starts here
   try {
-    // Get user input
-    const { first_name, last_name, email, password } = req.body;
+    const { user_name, email, password } = req.body;
 
-    // Validate user input
-    if (!(email && password && first_name && last_name)) {
+    if (!(email && password && user_name)) {
       res.status(400).send("All input is required");
     }
 
-    // check if user already exist
-    // Validate if user exist in our database
     const oldUser = await User.findOne({ email });
 
     if (oldUser) {
       return res.status(409).send("User Already Exist. Please Login");
     }
 
-    //Encrypt user password
     encryptedPassword = await bcrypt.hash(password, 10);
 
-    // Create user in our database
     const user = await User.create({
-      first_name,
-      last_name,
-      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      user_name,
+      email: email.toLowerCase(),
       password: encryptedPassword,
     });
 
-    // Create token
     const token = jwt.sign(
       { user_id: user._id, email },
       process.env.TOKEN_KEY,
@@ -57,34 +48,26 @@ app.post("/register", async (req, res) => {
         expiresIn: "2h",
       }
     );
-    // save user token
     user.token = token;
 
-    // return new user
     res.status(201).json(user);
   } catch (err) {
     console.log(err);
   }
-  // Our register logic ends here
 });
 
 
 app.post("/login", async (req, res) => {
 
-  // Our login logic starts here
   try {
-    // Get user input
     const { email, password } = req.body;
 
-    // Validate user input
     if (!(email && password)) {
       res.status(400).send("All input is required");
     }
-    // Validate if user exist in our database
     const user = await User.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
       const token = jwt.sign(
         { user_id: user._id, email },
         process.env.TOKEN_KEY,
@@ -93,17 +76,14 @@ app.post("/login", async (req, res) => {
         }
       );
 
-      // save user token
       user.token = token;
 
-      // user
       res.status(200).json(user);
     }
     res.status(400).send("Invalid Credentials");
   } catch (err) {
     console.log(err);
   }
-  // Our register logic ends here
 });
 
 module.exports = app;
