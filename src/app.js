373db68e72e2e26f99cc440b7d0c8f99f1c4c11a
RicardoMSFaria <RicardoMSFaria@users.noreply.github.com>
2021-11-22 16:08:30 +0000

    revoke and refresh

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ const express = require('express')
 
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const randToken = require('rand-token')
 
 const config = require('config')
 const auth = require('./middleware/auth')
 const userModel = require('./model/user')
 
 const tokenExpirationTime = config.token.expireIn
+var refreshTokensUsed = {}
 
 const app = express()
 
@@ -42,7 +44,7 @@ app.post('/register', async (req, res) => {
     })
 
     const token = jwt.sign(
-      { user_id: user._id, email },
+      { user_id: user._id, email, role },
       process.env.TOKEN_KEY,
       {
         expiresIn: tokenExpirationTime
@@ -68,14 +70,16 @@ app.post('/login', async (req, res) => {
 
     if (user && (await bcrypt.compare(password, user.password))) {
       const token = jwt.sign(
-        { user_id: user._id, email },
+        { user_id: user._id, email, role },
         process.env.TOKEN_KEY,
         {
-          expiresIn: '2h'
+          expiresIn: tokenExpirationTime
         }
       )
-
+      var refreshToken = randToken.uid(256);
       user.token = token
+      user.refreshToken = refreshToken
+      refreshTokensUsed[refreshToken] = user.name
 
       return res.status(200).json(user)
     }
@@ -85,5 +89,83 @@ app.post('/login', async (req, res) => {
     console.log(err)
   }
 })
+
+app.post('/refreshToken', function(req, res){
+  try {
+    const { email, refreshToken } = req.body
+
+    if (!(email && refreshToken)) {
+      return res.status(400).send('Required data missing')
+    }
+
+    if((refreshToken in refreshTokensUsed) && (refreshTokensUsed[refreshToken] == email)){
+      const user = await userModel.findOne({ email })
+
+      if(user.role !== "admin"){
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+      const token = jwt.sign(
+        { user_id: user._id, email, role },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: tokenExpirationTime
+        }
+      )
+
+      user.token = token
+      user.refreshToken = refreshToken
+      refreshTokensUsed[refreshToken] = user.name
+
+      return res.status(200).json(user)
+    }
+    res.status(400).send('Invalid data')
+  }
+  catch (err) {
+    console.log(err)
+  }
+})
+
+app.post('/revoke', auth, function(req, res){
+  try {
+    if (req.user.role !== "admin") {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    if(refreshToken in refreshTokensUsed){
+      delete refreshTokensUsed[refreshToken]
+    }
+    res.status(200).send("Token revoked")
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+app.post('/revokeAll', auth, function(req, res){
+  try {
+    if (req.user.role !== "admin") {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    for(var refreshToken in refreshTokensUsed){
+      delete refreshTokensUsed[refreshToken]
+    }
+    res.status(200).send("All tokens revoked");
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+app.get('/:id', auth, function(req, res){
+  try {
+    if (req.params.id !== req.user.id && req.user.role !== "admin") {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const user = await userModel.findById(req.params.id)
+    if (!user) return res.status(401).json({ message: 'User not found' });
+    return user;
+
+  }catch (err) {
+    console.log(err)
+  }
+});
 
 module.exports = app
